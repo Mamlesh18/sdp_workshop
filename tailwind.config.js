aed62ba04dd0d6a67ef599da0b093a6e6971f214
Mamlesh18 <113204216+Mamlesh18@users.noreply.github.com>
2023-11-07 16:11:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontSize: {
+      title: `2.6rem;`,
+      paragraph: `1.2rem;`
+    },
+    extend: {
+      colors: {
+        primary: {
+          500: '#FF6363;',
+          800: '#FF1313;',
+        }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
